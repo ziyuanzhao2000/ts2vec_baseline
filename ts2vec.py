25d857dd0ce55742869937f350a0c71338b450f7
Ziyuan Zhao <zhaoziyuan2000@gmail.com>
2022-05-01 01:38:41 -0400

    fix

--- ts2vec.py
+++ ts2vec.py
@@ -100,9 +100,9 @@ class TS2Vec:
             n_epoch_iters = 0
             interrupted = False
             for batch in train_loader:
-#                 if n_iters is not None and self.n_iters >= n_iters:
-#                     interrupted = True
-#                     break
+                if n_iters is not None and self.n_iters >= n_iters:
+                    interrupted = True
+                    break
                 
                 x = batch[0]
                 if self.max_train_length is not None and x.size(1) > self.max_train_length:
@@ -143,8 +143,8 @@ class TS2Vec:
                 
                 if self.after_iter_callback is not None:
                     self.after_iter_callback(self, loss.item())
-#             if interrupted:
-#                 break
+            if interrupted:
+                break
             
             cum_loss /= n_epoch_iters
             loss_log.append(cum_loss)
