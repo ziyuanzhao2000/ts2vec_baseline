c7c1c89ee11618c744f8979a5eabecdd61201c2d
Ziyuan Zhao <zhaoziyuan2000@gmail.com>
2022-05-01 00:22:11 -0400

    fix

--- tasks/classification.py
+++ tasks/classification.py
@@ -38,7 +38,7 @@ def eval_classification(model, train_data, train_labels, test_data, test_labels,
 #     print(train_labels.max())
 #     print(train_labels)
 #     test_labels_onehot = label_binarize(test_labels, classes=np.arange(train_labels.max()+1))
-    test_labels_onehot = (F.one_hot(torch.tensor(test_labels), num_classes=int(train_labels.max()+1))).numpy()
+    test_labels_onehot = (F.one_hot(torch.tensor(test_labels).long(), num_classes=int(train_labels.max()+1))).numpy()
 #     print(test_labels_onehot)
 #     auprc = average_precision_score(test_labels_onehot, y_score)
     print(test_labels_onehot.shape, y_score.shape)
